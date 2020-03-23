ef563af855c7bdb4ce790cce6b1432c0d1a44398
Alex Marshall <alex.k.marshall83@gmail.com>
2020-03-23 15:10:23 +0000

    Automatically backed up by Learn

--- lib/my_code.rb
+++ lib/my_code.rb
@@ -7,8 +7,8 @@ def map(source)
   result
 end
 
-def reduce(source)
-  acc = 0
+def reduce(source, initial_value = 0)
+  acc = initial_value
   source.length.times do |index|
     acc = yield(acc, source[index])
   end
